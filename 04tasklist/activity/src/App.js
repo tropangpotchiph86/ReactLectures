dc39e7277e4f63d73795bb1d1224d318cc6e3016
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-19 14:11:17 +0800

    React Lectures

---
+++ 04tasklist/activity/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 5271, name: 'Record React Lectures', completed: true },
+    { id: 7825, name: 'Edit React Lectures', completed: false },
+    { id: 8391, name: 'Watch Lectures', completed: false },
+  ]);
+
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    // console.log(id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  return (
+    <div className="App">
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>
+              {task.id} - {task.name}
+            </span>
+            <button
+              onClick={() => handleDelete(task.id)}
+              className="delete"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
