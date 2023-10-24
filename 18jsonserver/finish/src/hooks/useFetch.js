d1f494694b24d608beb620e59ecd22f59641925a
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-24 11:02:07 +0800

    React Lectures

---
+++ 18jsonserver/finish/src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const result = await response.json();
+        setLoading(false);
+        setData(result);
+        setError('');
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+      }
+    };
+    fetchData();
+    return () => controller.abort();
+  }, [url]);
+
+  return { data, loading, error };
+};
